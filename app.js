93db71026ec7fb843bde21fa464552160135d939
Boatdk <boatz.dk@gmail.com>
2018-07-21 15:35:46 +0700

    updated

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ var app = express()
 var bodyParser = require('body-parser');
 var morgan = require('morgan')
 var methodOverride = require('method-override')
-var doQuery = require('./utils/doQuery')
-var con = require('./config/route')
+const getUser = require('./test/user').getUser
+const getEbouchure = require('./test/ebouchure').getEbouchure
+const postEbouchure = require('./test/ebouchure').postEbouchure
+const putEbouchure = require('./test/ebouchure').putEbouchure
+const deleteEbouchure = require('./test/ebouchure').deleteEbouchure
+const postUser = require('./test/user').postUser;
+const deleteUser = require('./test/user').deleteUser
+const putUser = require('./test/user').putUser
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -15,47 +21,66 @@ app.use(morgan('dev'))
 app.use(methodOverride())
 app.use('/uploads', express.static('uploads'))
 
-app.get('/showForm', (req, res) => {
-    res.sendFile(__dirname + "/" + "showForm.html")
-})
-
 app.set('view engine', 'ejs')
-
 
 app.get('/', (req, res) => {
     res.render('pages/index')
 })
 
-
 app.get('/user', (req, res) => {
-    let sql = `SELECT * FROM users`
-    doQuery(sql).then((resp) => {
-        res.render('pages/user', {
-            data: resp
-        })
-    })
+   return getUser().then((resp) => {
+       res.render('pages/user', {
+           data: resp
+       })
+   })
 })
 
 app.get('/user/create', (req, res) => {
     res.render('pages/addUser')
 })
 
-app.get('/ebouchure', (req, res) => {
-    let sql = `SELECT * FROM ebouchures`
-    doQuery(sql).then((resp) => {
-        res.render('pages/ebouchure', {
-            data: resp
-        })
-    })  
+app.post('/user/create/creating', (req, res) => {
+    let body = req.body
+    let data = {
+        firstname : body.firstname,
+        lastname :  body.lastname,
+        username:  body.username,
+        password:  body.password,
+        email: body.email,
+        tel: body.tel,
+        privilege: "users"
+      }
+    return postUser(data).then((resp) => {
+        res.redirect('/user')
+    })
 })
 
-app.get('/showData', (req, res) => {
-    data = {
-        fname: req.query.fname,
-        lname: req.query.lname
-    }
-    console.log(data)
-    res.end(JSON.stringify(data))
+app.get('/user/update', (req, res) => {
+    res.render('pages/updateUser')
+})
+
+app.post('/user/update/updating', (req, res) => {
+    let body = req.body
+    let data = {
+            firstname : body.firstname,
+            lastname :  body.lastname,
+            username:  body.username,
+            password:  body.password,
+            email: body.email,
+            tel: body.tel,
+            privilege: "users"
+        }
+    return putUser(data).then((resp) => {
+
+    })
+})
+
+app.get('/ebouchure', (req, res) => {
+    return getEbouchure().then((resp) => {
+        res.render('pages/ebouchure' ,{
+            data: resp
+        })
+    })
 })
 
 app.use((req, res, next) => {
